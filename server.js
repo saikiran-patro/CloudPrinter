ce8f8ccefa3a7f182f0c1468e18822319ef48d5b
Sai Kiran Patro <saikiranpatro198@gmail.com>
2022-05-05 23:20:56 +0530

    dev stage

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 import express from 'express';
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({
     extended: true
   }));
+
+mongoose.connect("mongodb://localhost:27017/cpAdidb", {useNewUrlParser: true},{ useUnifiedTopology: true } );
 app.set('view engine', 'ejs');
 
+
+
+const adminSchema=new mongoose.Schema({
+  AdminID:String,
+  Password:String,
+  
+
+})
+const Admin= mongoose.model("Admin",adminSchema);
 app.get('/',(req, res) => {
-   res.render('sign')
+   res.render('sign',{flag:0})
 })
 app.post('/signin',(req, res) => {
-  res.render('Admin');
+
+  const id = req.body.id;
+  const password = req.body.password
+  Admin.findOne({AdminID: id, Password: password},(err, foundUser)=>{
+
+      if(err){
+          console.log(err);
+
+      }
+      else{
+          if(foundUser){
+            res.render('Admin');
+          }
+          else{
+              
+            res.render('sign',{flag:1})
+          }
+      }
+
+  })
+ 
 })
 
 app.get("/printerdetails",(req, res) => {
